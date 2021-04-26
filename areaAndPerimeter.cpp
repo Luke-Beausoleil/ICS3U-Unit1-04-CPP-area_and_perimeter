bfa33db58a167ea085ab852e8542a5ed2e41edd4
Luke Beausoleil <luke.beausoleil@mths.ca>
2021-04-26 03:02:44 +0000

    completed area/permieter program

---
+++ areaAndPerimeter.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 Luke Beausoleil All rights reserved
+//
+// Created by: Luke Beausoleil
+// Created on: April 2021
+// This program calculates the perimeter and the area of a (continued)
+// rectangle with dimensions 5cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has dimensions" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << 5 * 3 << "cm^2" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Perimeter is " << 2*(5+3) << "cm" << std::endl;
+}
